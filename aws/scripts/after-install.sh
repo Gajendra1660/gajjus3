4d8780d9b0920f63b7edd61a4ae7d4401c29c854
gajju <example.com>
2023-05-06 12:20:56 +0530

    test

---
+++ aws/scripts/after-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -xe
+
+
+# Copy war file from S3 bucket to tomcat webapp folder
+aws s3 cp s3://codedeploystack1-webappdeploymentbucket-w5sakxj9eg34
+# Ensure the ownership permissions are correct.
+chown -R tomcat:tomcat /usr/local/tomcat9/webapps
